aa93bfd02c337e53292b8b1d5657f33f97e5b3b3
JMT2080AD <jason.morgan.taylor@gmail.com>
2017-11-27 21:08:02 -0800

    problematic update. need to fix automatic defaulting issues.

--- server_ui.r
+++ server_ui.r
@@ -7,7 +7,7 @@ library(shiny)
 library(ggplot2)
 
 ## read in county polygons and melt
-counties <- st_read("./data_input", "cb_2016_us_county_20m", quiet = T)
+counties <- st_read("./data_input", "cb_2016_us_county_20m", quiet = T, stringsAsFactors = F)
 counties <- st_transform(counties, 2285)
 
 counties <- counties[counties$STATEFP == 53,]
@@ -27,12 +27,7 @@ counties.points <- rbindlist(mapply(function(x, geoid, name){
 ## read in base data
 dat <- readRDS("./data_output/washington_qcew.rds")
 
-## ## define plotting functions
-## x <- 1465873.67877331
-## y <- 416762.061959529
-## x <- NULL
-## y <- NULL
-
+## build color ramp palette
 crp <- colorRampPalette(c("red",
                           "yellow",
                           "green",
@@ -40,39 +35,51 @@ crp <- colorRampPalette(c("red",
 
 selectGeoid <- function(x, y){
     if(is.null(x)){
-        return("No county selected.")
+        return(53000)
     }
     counties$geoid[st_intersects(counties, st_point(c(x, y)), sparse = F)]
 }
 
 jobPlot <- function(geoid){
-    countyName <- dat[area_fips == geoid & periodName == "Annual", area_title, by = area_title]
+    countyName <- unique(dat[area_fips == geoid & periodName == "Annual"]$area_title)
     ggplot(dat[area_fips == geoid & periodName == "Annual",]) +
-    geom_line(aes(year, value, group = industry_title, colour = industry_title, linetype = industry_title)) +
-    theme_bw() +
-    scale_color_manual(values=crp(length(unique(dat$industry_title)))) + 
-    scale_linetype_manual(values=rep(c("solid", "dotdash", "dashed"), 4)) +
-    labs(title = "Average Weekly Income",
-         subtitle = paste("All Industry Groups Average Weekly Income -",  countyName),
-         caption = "Source: Bureau of Labor Statistics",
-         x = "Year",
-         y = "Average Weekly Income in Dollars")
-
+        geom_line(aes(year, value, group = industry_title, colour = industry_title, linetype = industry_title)) +
+        theme_bw() +
+        scale_color_manual(values=crp(length(unique(dat$industry_title)))) + 
+        scale_linetype_manual(values=rep(c("solid", "dotdash", "dashed"), 4)) +
+        labs(title = "Average Weekly Income",
+             subtitle = paste("All Industry Groups Average Weekly Income -",  countyName),
+             caption = "Source: Bureau of Labor Statistics",
+             x = "Year",
+             y = "Average Weekly Income in Dollars")
 }
 
-washMap <- function(){
+washMap <- function(geoid_select){
+    if(is.null(geoid_select)){
+        countyName <- "No county selected"
+    }else{
+        countyName <- unique(dat[area_fips == geoid_select & periodName == "Annual"]$area_title)
+    }
     eb <- element_blank()
-    ggplot(counties.points) +
-        geom_polygon(aes(x, y, group = name), fill = "white", color = "black") +
+    ggplot() +
+        geom_polygon(data = counties.points[geoid != geoid_select,],
+                     aes(x, y, group = name),
+                     fill = "white",
+                     color = "black") +
+        geom_polygon(data = counties.points[geoid == geoid_select,],
+                     aes(x, y, group = name),
+                     fill = "grey",
+                     color = "black") + 
         coord_equal() +
+        ggtitle(countyName) + 
         guides(fill = F) +
-        title("County") + 
-        theme(panel.border = eb,
-              panel.grid = eb,
+        theme(plot.title       = element_text(hjust = 0.5),
+              panel.border     = eb,
+              panel.grid       = eb,
               panel.background = eb,
-              axis.title = eb,
-              axis.text  = eb,
-              axis.ticks = eb)
+              axis.title       = eb,
+              axis.text        = eb,
+              axis.ticks       = eb)
 }
 
 ## run server
@@ -86,7 +93,7 @@ ui <- fluidPage(
 
 server <- function(input, output){
     output$map_plot <- renderPlot(
-        washMap()
+        washMap(selectGeoid(input$maplocation$x, input$maplocation$y))
     )
     output$line_plot <- renderPlot(
         jobPlot(selectGeoid(input$maplocation$x, input$maplocation$y))
